57bd7c8e6f7c1712a59c3db5169c562ed22d6abf
Giully314 <giully@protonmail.com>
2022-01-14 16:42:01 +0100

    premake

--- premake5.lua
+++ premake5.lua
@@ -7,29 +7,43 @@ project "glfw"
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
 
+	--https://stackoverflow.com/questions/70199224/trouble-linking-with-glfw-using-premake-and-vs2019
 	files
 	{
 		"include/GLFW/glfw3.h",
 		"include/GLFW/glfw3native.h",
-		"src/glfw_config.h",
+		"src/internal.h",
+		"src/platform.h",
+		"src/mappings.h",
 		"src/context.c",
 		"src/init.c",
 		"src/input.c",
 		"src/monitor.c",
-		"src/window.c"
-	}
-
-	files
-	{
-		"src/win32_init.c",
-		"src/win32_joystick.c",
-		"src/win32_monitor.c",
-		"src/win32_time.c",
-		"src/win32_thread.c",
-		"src/win32_window.c",
-		"src/wgl_context.c",
+		"src/platform.c",
+		"src/vulkan.c",
+		"src/window.c",
 		"src/egl_context.c",
-		"src/osmesa_context.c"
+		"src/osmesa_context.c",
+		"src/null_platform.h",
+		"src/null_joystick.h",
+		"src/null_init.c",
+
+		"src/null_monitor.c",
+		"src/null_window.c",
+		"src/null_joystick.c",
+
+		"src/win32_init.c",
+        "src/win32_module.c",
+        "src/win32_joystick.c",
+        "src/win32_monitor.c",
+        "src/win32_time.h",
+        "src/win32_time.c",
+        "src/win32_thread.h",
+        "src/win32_thread.c",
+        "src/win32_window.c",
+        "src/wgl_context.c",
+        "src/egl_context.c",
+        "src/osmesa_context.c"
 	}
 
 	defines 
